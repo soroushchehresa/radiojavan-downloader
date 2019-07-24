d2889ad8976e228ed665ce656ede95f3b6f8d9bf
Unknown <s1996ch@gmail.com>
2019-07-24 18:58:54 +0430

    Fix bugs

--- background.js
+++ background.js
@@ -1,33 +1,36 @@
-(function (listener) {
+(function() {
   let buttonLink;
   let downloadLink256;
   let downloadLink320;
   let loading;
+
   function handleAddDownloadButtons() {
-    buttonLink = '';
-    downloadLink256 = '';
-    downloadLink320 = '';
-    loading = false;
-    if (!loading && (!downloadLink320 || (buttonLink !== downloadLink320))) {
-      loading = true;
-      let formData = new FormData();
-      formData.append('id', 'Koorosh-Yebaram-Man-(Ft-Arta-Behzad-Leito-Raha)');
-      let request = new XMLHttpRequest();
-      request.open('POST', 'https://www.radiojavan.com/mp3s/mp3_host');
-      request.onload = function () {
-        if (request.status === 200) {
-          chrome.tabs.query({ 'active': true }, function (tabs) {
-            let parsedUrl = new URL(tabs[0].url);
-            if (parsedUrl.origin === 'https://www.radiojavan.com') {
-              let urlParts = parsedUrl.pathname.split('/');
-              let musicName = urlParts[3];
-              if (urlParts[1] === 'mp3s' || 'podcasts') {
-                let contentType = urlParts[1].slice(0, -1);
-                downloadLink256 = `${JSON.parse(request.response).host}/media/${contentType}/mp3-256/${musicName}.mp3`;
-                downloadLink320 = `${JSON.parse(request.response).host}/media/${contentType}/mp3-320/${musicName}.mp3`;
-                buttonLink = downloadLink320;
-                chrome.tabs.executeScript({
-                  code: `
+    chrome.tabs.query({ 'active': true }, (tabs) => {
+      buttonLink = '';
+      downloadLink256 = '';
+      downloadLink320 = '';
+      let parsedUrl = new URL(tabs[0].url);
+      let urlParts = parsedUrl.pathname.split('/');
+      let musicName = urlParts[3];
+      loading = false;
+      if (
+        !loading &&
+        (!downloadLink320 || (buttonLink !== downloadLink320)) &&
+        (urlParts[1] === 'mp3s' || 'podcasts')
+      ) {
+        loading = true;
+        let formData = new FormData();
+        formData.append('id', musicName);
+        let request = new XMLHttpRequest();
+        request.open('POST', `https://www.radiojavan.com/${urlParts[1] === 'mp3s' ? 'mp3s/mp3_host' : 'podcasts/podcast_host'}`);
+        request.onload = function() {
+          if (request.status === 200) {
+              let contentType = urlParts[1].slice(0, -1);
+              downloadLink256 = `${JSON.parse(request.response).host}/media/${contentType}/mp3-256/${musicName}.mp3`;
+              downloadLink320 = `${JSON.parse(request.response).host}/media/${contentType}/mp3-320/${musicName}.mp3`;
+              buttonLink = downloadLink320;
+              chrome.tabs.executeScript({
+                code: `
                       if (!document.getElementById('download-container')) {
                         var html = '<div id="download-container" style="text-align: center;width: 100%;height: 25px;margin-top: 30px"><a target="_blank" style="color: white; background: #aa2712;padding: 10px 20px;margin: 10px;border-radius: 5px;" href="${downloadLink256}">Download 256</a><a target="_blank" style="color: white; background: #aa2712;padding: 10px 20px;margin: 10px;border-radius: 5px;" href="${downloadLink320}">Download 320</a></div>';
                         var div = document.createElement('div');
@@ -37,20 +40,19 @@
                         }
                       }
                   `,
-                });
-              }
-              loading = false;
-            }
-          });
-        }
-      };
-      request.send(formData);
-    }
+              });
+            loading = false;
+          }
+        };
+        request.send(formData);
+      }
+    });
   }
-  chrome.webNavigation.onDOMContentLoaded.addListener((details) => {
+
+  chrome.webNavigation.onDOMContentLoaded.addListener(() => {
     handleAddDownloadButtons();
   }, { url: [{ hostSuffix: 'radiojavan.com' }] });
-  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
     if (changeInfo.url) {
       handleAddDownloadButtons();
     }
